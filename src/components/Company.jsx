e54542dc63ce4a134b951a782fc69ada21fcb7bb
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2025-04-01 13:48:41 +0530

    Update Company.jsx

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -18,28 +18,6 @@ function Company() {
     });
   }, []);
   return (
-    <section
-      id="company"
-      className="flex h-full w-full flex-col items-center justify-center gap-8 bg-main px-6 py-8 text-white lg:px-20 xl:gap-16"
-    >
-      <div className="company-grid grid w-full grid-cols-2 place-content-between place-items-center gap-12 md:grid-cols-3 md:gap-16 lg:grid-cols-6">
-        <div>
-          <img src="/assets/brands/slack.webp" alt="Mission CS Test Series" />
-        </div>
-        <div>
-          <img src="/assets/brands/google.webp" alt="Chartered team" />
-        </div>
-        <div>
-          <img src="/assets/brands/netflix.webp" alt="Hotel Hilton" />
-        </div>
-        <div>
-          <img src="/assets/brands/airbnb.webp" alt="My CS MTP" />
-        </div>
-        <div>
-          <img src="/assets/brands/adobe.webp" alt="Delhi Bakery School" />
-        </div>
-      </div>
-    </section>
   );
 }
 
